573b95757dcc070364ffb92559a666358593dbd2
vonzhou <vonzhou@163.com>
2019-01-31 12:53:55 +0800

    title

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ if "Windows" not in p:
 
 # 处理写UTF-8到文件，需要这样
 file = codecs.open("index.html","w", "utf-8") 
-start = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta http-equiv="X-UA-Compatible" content="ie=edge" /><title>Document</title></head><body>""" 
+start = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta http-equiv="X-UA-Compatible" content="ie=edge" /><title>编程之路 by vonzhou</title></head><body>""" 
 end = """</body></html>"""
 
 # 1.写头部
